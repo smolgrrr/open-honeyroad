74404670f1d6cdb5cf187d0fd8398f3a6270ae50
DESKTOP-\marco <marcojones1594@gmail.com>
2022-09-27 11:04:16 +0100

    touch up convert page

--- src/pages/Convert.jsx
+++ src/pages/Convert.jsx
@@ -1,25 +1,62 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import AsyncImage from '../components/AsyncImage'
+import { createUseStyles } from 'react-jss'
 
-const Convert = props => {
+const useStyles = createUseStyles((theme) => ({
+  fixedFloat: {
+    flexDirection: "column",
+    display: "flex",
+    width: "fit-content",
+
+    "@media screen and (max-width: 768px)": {
+      margin: "0 auto",
+    },
+  },
+  img: {
+    borderRadius: "5px",
+    overflow: "hidden",
+  },
+  titleIFrame: {
+    display: "inline-block",
+    verticalAlign: "top",
+    marginTop: "15px",
+  },
+  IFrame: {
+    minHeight: "474px",
+    width: "100%",
+    border: "none",
+  },
+}))
+
+const Convert = (props) => {
+  const classes = useStyles()
   return (
     <div className="fragment">
       <div id="all_but_footer">
         <div id="content">
-          <div className="main-info-of-product">
-            <span className="img_one_wrapper">
-              <div className="async-image-placeholder" style={{width: '100%', height: '28vw',}}>
+          <div className={classes.fixedFloat + " main-info-of-product"}>
+            <span className={classes.img}>
+              <div
+                className="async-image-placeholder"
+                style={{ width: "100%", height: "28vw" }}
+              >
                 <AsyncImage
                   src="fixedfloat.jpeg"
-                  style={{width: '100%', height: '100%',}}
+                  style={{ width: "100%", height: "100%" }}
                 />
               </div>
             </span>
-            <span style={{display: 'inline-block', verticalAlign: 'top'}}>
-              <span className="h1">Swap to <i className="fak fa-satoshisymbol-solid" /></span>
+            <span className={classes.titleIFrame}>
+              <span className="h1">
+                Swap to <i className="fak fa-satoshisymbol-solid" />
+              </span>
               <br />
               <br />
-              <iframe src="https://widget.fixedfloat.com/?to=BTCLN&amp;ref=4fg6te1a&amp;lockReceive=true&amp;lockType=true&amp;hideType=true&amp;lockAmount=true&amp;toAmount=0.001" allowtransparency="true" style={{minHeight: '400px', width: '100%', border: 'none'}}></iframe>
+              <iframe
+                className={classes.IFrame}
+                src="https://widget.fixedfloat.com/?to=BTCLN&amp;ref=4fg6te1a&amp;lockReceive=true&amp;lockType=true&amp;hideType=true&amp;lockAmount=true&amp;toAmount=0.001"
+                allowtransparency="true"
+              ></iframe>
               <br />
             </span>
           </div>
